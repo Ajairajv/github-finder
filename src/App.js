3fbba1d41a8915ccaf6cbd1bd7cb5b0a46424270
Ajai <armmjvs@gmail.com>
2023-12-01 21:10:37 +0530

    nav-bar
    nav-bar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import  {BrowserRouter as Router ,Route} from 'react-router-dom'
+import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
-    <div className="bg-red-500">
-    <h1 className='text-xl text-center'>Hello World</h1>
-    <button className="btn">click</button>
-    </div>
+      <Router>
+         <div className="flex flex-col justify-between h-screen">
+             <Navbar/>
+             <main>
+              Content
+             </main>
+
+
+         </div>
+      </Router>
   );
 }
-
 export default App;
